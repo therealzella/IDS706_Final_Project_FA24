9261b5c1d138d5ec6301b8dda20cda4f251775df
VCM admin account <vcm@vcm-44481.vm.duke.edu>
2024-12-07 23:50:22 -0500

    simplify home page

--- app_pages/function.py
+++ app_pages/function.py
@@ -1,11 +1,8 @@
 import json
-
 import streamlit as st
-
 from services import analyze
 from configs import OPENAI_GPT3, OPENAI_GPT4, REVIEW_NUM_CAP, OPENAI_CAP, ANALYSIS_FOCUS, USER_POSITION, CONTENT_COL_CONFIG
 from services.filereader_en import FileReader
-from style.color_theme import html_header_color_1
 
 # --------------------------------------------------------------------------------
 # ---- FUNCTION PAGE -------------------------------------------------------------
@@ -16,11 +13,11 @@ with open('app_pages/function_lang.json', 'r', encoding='utf-8') as f:
     texts = json.load(f)
 
 def show_function_page():
+    head_color = "#166088"
     _, center, _ = st.columns(CONTENT_COL_CONFIG)
 
     with center:
-        st.markdown(f"<h2 style='text-align: center; color: {html_header_color_1};'>{texts['center_subtitle1']}</h2>", unsafe_allow_html=True)
-        st.markdown("<h6 style='text-align: center; color: grey;'>No Login Required</h6>", unsafe_allow_html=True)
+        st.markdown(f"<h2 style='text-align: center; color: {head_color};'>{texts['center_subtitle1']}</h2>", unsafe_allow_html=True)
         step1_block = st.container()
         st.write("---")
         step2_block = st.container()
@@ -30,7 +27,7 @@ def show_function_page():
         # --- STEP1: File Preperation Instruction ---
         with step1_block:
             
-            st.markdown(f"<h4 style='color: {html_header_color_1};'>{texts['step_one']}</h4>", unsafe_allow_html=True)
+            st.markdown(f"<h4 style='color: {head_color};'>{texts['step_one']}</h4>", unsafe_allow_html=True)
             st.write(texts['step_one_subtitle'])
 
             review_download_instruction = st.expander(texts['step_one_instruction'])
@@ -43,11 +40,10 @@ def show_function_page():
 
         # --- STEP2: File Upload and Validation Check ---
         with step2_block:
-            st.markdown(f"<h4 style='color: {html_header_color_1};'>{texts['step_two_markdown']}</h4>", unsafe_allow_html=True)
+            st.markdown(f"<h4 style='color: {head_color};'>{texts['step_two_markdown']}</h4>", unsafe_allow_html=True)
 
             step2_col2, step2_col1 = st.columns((2, 4))
 
-            # 文件上传及合法性检验
             with step2_col1: 
                 uploaded_file = st.file_uploader(label=texts['step_two_uploaded'], type="xlsx")
                 file = FileReader(uploaded_file)
@@ -77,7 +73,7 @@ def show_function_page():
 
         # --- STEP3: Analysis Options ---
         with step3_block:
-            st.markdown(f"<h4 style='color: {html_header_color_1};'>{texts['step_three_markdown']}</h4>", unsafe_allow_html=True)
+            st.markdown(f"<h4 style='color: {head_color};'>{texts['step_three_markdown']}</h4>", unsafe_allow_html=True)
 
             st.markdown(f"<h6>{texts['step_three_markdown2']}</h6>", unsafe_allow_html=True)
             step3a_col1, step3a_col2, step3a_col3 = st.columns((4, 2, 2))
@@ -88,8 +84,6 @@ def show_function_page():
             with step3a_col3:
                selected_position = st.selectbox(texts['step_three_selected_position'], USER_POSITION)
 
-            
-            # 高级分析选项
             advanced_options = st.expander(texts['step_three_advanced_options'])
 
             with advanced_options: 
@@ -129,11 +123,5 @@ def show_function_page():
                         input_question,
                         )
                     analyze.gpt_stream_completion(prompt, model=OPENAI_GPT3)
-                    # if selected_model == OPENAI_GPT3 or selected_model == OPENAI_GPT4: 
-                    #     analyze.gpt_stream_completion(prompt, model=selected_model)
-                    # else: 
-                    #     if selected_model == texts['step_three_selected_model3']:
-                    #         selected_model = OPENAI_GPT3
-                    #     analyze.claude_stream_completion(prompt, model=selected_model)
                         
                 st.markdown(f"</br></br></br></br><p style='text-align: center; color: #BFBFBF; font-size: 16px;'> Powered by OpenAI {selected_model}</p>", unsafe_allow_html=True)
--- app_pages/home.py
+++ app_pages/home.py
@@ -1,6 +1,5 @@
 import random
 import time
-import json
 import streamlit as st
 from configs import ANALYSIS_FOCUS, CONTENT_COL_CONFIG, USER_POSITION
 from style.color_theme import html_header_color_1
@@ -9,14 +8,8 @@ from utils.image_loader import img_to_bytes, img_to_html
 # --------------------------------------------------------------------------------
 # ---- HOME PAGE -----------------------------------------------------------------
 # --------------------------------------------------------------------------------
-
-# multi-lingual JSON dictionary
-with open('app_pages/home_lang.json', 'r', encoding='utf-8') as f:
-    texts = json.load(f)
  
 def show_home_page():
-
-    INSIGHTFUL_REVIEWS = texts['insightful_reviews']
     INSIGHTFUL_REVIEWS = "Review Analyzer"
 
     _, center, _ = st.columns(CONTENT_COL_CONFIG)
@@ -29,10 +22,10 @@ def show_home_page():
 
     # --- Feature 1: Time Saving ---
     st.markdown(f"""<h2 style='text-align: center; color: {html_header_color_1};'>
-                {texts['time_saving_subtitle']}</h2>
+                {"Comprehensive Review Analysis in 30 Seconds"}</h2>
                 """, unsafe_allow_html=True)
     st.markdown(f"""<h3 style='text-align: center; line-height: 2;'>
-                {texts['time_saving_description']}</h3>
+                {"Leveraging cutting-edge large language models </br>Process up to 1000 product reviews in 30 seconds </br>Save 30-60 minutes of operation time daily"}</h3>
                 """, unsafe_allow_html=True)
 
     _, center, _ = st.columns(CONTENT_COL_CONFIG)
@@ -42,22 +35,31 @@ def show_home_page():
 
     # --- Feature 2: Tailored Insights ---
     st.markdown(f"""<h2 style='text-align: center; color: {html_header_color_1};'>
-                {texts['tailored_insights_subtitle']}</h2>""", 
+                {"Provide Customized Analysis Results Tailored to Your Needs"}</h2>""", 
                 unsafe_allow_html=True)
-    rolling_content = st.empty()
-    with rolling_content: 
-        for i in range(8): 
-            random_position = list(USER_POSITION)[random.randint(1, len(USER_POSITION) - 1)]
-            random_focus = list(ANALYSIS_FOCUS)[random.randint(1, len(ANALYSIS_FOCUS) - 1)]
-            position_focus_message = texts['position_and_focus'].format(random_position, random_focus)
-            tailored_insights_final_message = texts['position_and_focus'].format("👩🏻‍🚀any position", "🌟any aspect")
-            st.markdown(f"""<h3 style='text-align: center; line-height: 2;'>
-                        {position_focus_message}
-                        </h3>""", unsafe_allow_html=True)
-            time.sleep(0.4)
-        st.markdown(f"""<h3 style='text-align: center; line-height: 2;'>
-                    {tailored_insights_final_message}
-                    </h3>""", unsafe_allow_html=True)
+    st.markdown(f"""<h3 style='text-align: center; line-height: 2;'>
+            {"No matter you are in any position,"}</h3>
+            """, unsafe_allow_html=True)
+    st.markdown(f"""<h3 style='text-align: center; line-height: 2;'>
+            {"or you are interested in any aspect,"}</h3>
+            """, unsafe_allow_html=True)
+    st.markdown(f"""<h3 style='text-align: center; line-height: 2;'>
+            {"we provide you with customized analysis"}</h3>
+            """, unsafe_allow_html=True)
+    # rolling_content = st.empty()
+    # with rolling_content: 
+    #     for i in range(8): 
+    #         random_position = list(USER_POSITION)[random.randint(1, len(USER_POSITION) - 1)]
+    #         random_focus = list(ANALYSIS_FOCUS)[random.randint(1, len(ANALYSIS_FOCUS) - 1)]
+    #         position_focus_message = texts['position_and_focus'].format(random_position, random_focus)
+    #         tailored_insights_final_message = texts['position_and_focus'].format("any position", "any aspect")
+    #         st.markdown(f"""<h3 style='text-align: center; line-height: 2;'>
+    #                     {position_focus_message}
+    #                     </h3>""", unsafe_allow_html=True)
+    #         time.sleep(0.4)
+    #     st.markdown(f"""<h3 style='text-align: center; line-height: 2;'>
+    #                 {tailored_insights_final_message}
+    #                 </h3>""", unsafe_allow_html=True)
 
     _, center, _ = st.columns(CONTENT_COL_CONFIG)
     with center: 
@@ -65,15 +67,14 @@ def show_home_page():
 
 
     # --- Feature 3: Compatible with All Major EC Platforms ---
-    st.markdown(f"""<h2 style='text-align: center; color: {html_header_color_1};'>
-                {texts['ecommerce_compatibility_subtitle']}</h2>
-                """, unsafe_allow_html=True)
-    st.markdown("<h6 style='text-align: center; color: grey;'>Development in Progress</h6>", unsafe_allow_html=True)
-    _, center, _ = st.columns(CONTENT_COL_CONFIG)
-    with center: 
-        supported_ec_sites = img_to_bytes('assets/Supported_EC_Sites.png')
-        st.markdown(f"""
-            <div style="text-align: center;">
-                <img src="data:image/png;base64,{supported_ec_sites}" style="width: 80%; max-width: 1500px;">
-            </div>
-            """, unsafe_allow_html=True)
+    # st.markdown(f"""<h2 style='text-align: center; color: {html_header_color_1};'>
+    #             {texts['ecommerce_compatibility_subtitle']}</h2>
+    #             """, unsafe_allow_html=True)
+    # _, center, _ = st.columns(CONTENT_COL_CONFIG)
+    # with center: 
+    #     supported_ec_sites = img_to_bytes('assets/Supported_EC_Sites.png')
+    #     st.markdown(f"""
+    #         <div style="text-align: center;">
+    #             <img src="data:image/png;base64,{supported_ec_sites}" style="width: 80%; max-width: 1500px;">
+    #         </div>
+    #         """, unsafe_allow_html=True)
